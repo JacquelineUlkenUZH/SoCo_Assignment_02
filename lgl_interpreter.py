24121e9ba185cbe561a6aa9aa2f3d003d58ec81a
JacquelineUlkenUZH <57492136+JacquelineUlkenUZH@users.noreply.github.com>
2023-11-01 16:18:10 +0100

    Add do() function

--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -1,1 +1,11 @@
+OPS = {name.replace("do_", ""): func for (name, func) in globals().items() if name.startswith("do_")}
 
+def do(env, expr):
+    # Lists trigger function calls.
+    if isinstance(expr, list):
+      assert expr[0] in OPS, f"Unknown operation {expr[0]}"
+      func = OPS[expr[0]]
+      return func(env, expr[1:])
+    # Everything else returns itself
+    else:
+      return expr
