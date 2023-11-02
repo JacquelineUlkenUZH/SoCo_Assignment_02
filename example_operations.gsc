a2beca7dd2ffe9e18c53c5ab30b7803d34f64e54
luzmediach <marc@luzmedia.ch>
2023-11-02 12:27:59 +0100

    arithemtik, var, funktionen. check operations.gsc

--- example_operations.gsc
+++ example_operations.gsc
@@ -1,1 +1,7 @@
-
+[
+  "abfolge",
+  ["varsetzen", "meinefunktion", ["funktion", ["meinargument"], ["subtrahieren", 10, ["varabrufen", "meinargument"]]]],
+  ["varsetzen", "variabel_a", ["funkaufrufen", "meinefunktion", 3]],
+  ["varsetzen", "variabel_b", ["addieren", 2, 3]],
+  ["addieren", ["varabrufen", "variabel_a"], ["varabrufen", "variabel_b"]]
+]
--- lgl_interpreter.py
+++ lgl_interpreter.py
@@ -1,76 +1,76 @@
+import argparse
+import json
+import os
+
+abspath = os.path.dirname(os.path.abspath(__file__))
+os.chdir(abspath)
+
+###################################
+# From here we add do_ operations #
+###################################
+
+
 # Arithmetic
-def do_addieren(args):
+def do_addieren(envs, args):
     assert len(args) == 2
-    left = do(args[0])
-    right = do(args[1])
+    left = do(envs, args[0])
+    right = do(envs, args[1])
     return left + right
 
 
-def do_subtrahieren(args):
+def do_subtrahieren(envs, args):
     assert len(args) == 2
-    left = do(args[0])
-    right = do(args[1])
+    left = do(envs, args[0])
+    right = do(envs, args[1])
     return left - right
 
 
-def do_absolutwert(args):
+def do_absolutwert(envs, args):
     assert len(args) == 1
-    value = do(args[0])
+    value = do(envs, args[0])
     return abs(value)
 
 
-# Environment
-def envs_get(envs, name):
-    assert isinstance(name,str)
-    for e in reversed(envs):
-        if name in e:
-            return e[name]
-    assert False, f"Unknown variable name {name}"
-
-def envs_set(envs,name,value):
-    assert isinstance(name,str)
-    envs[-1][name] = value
-
-
 # Variables
-def do_setzen(envs,args):
+def do_varsetzen(envs, args):
     assert len(args) == 2
-    assert isinstance(args[0],str)
+    assert isinstance(args[0], str)
     var_name = args[0]
     value = do(envs, args[1])
     envs_set(envs, var_name, value)
     return value
 
-def do_abrufen(envs,args):
+
+def do_varabrufen(envs, args):
     assert len(args) == 1
-    return envs_get(envs,args[0])
+    return envs_get(envs, args[0])
 
 
 # Functions
-def do_funktion(envs,args):
+def do_funktion(envs, args):
     assert len(args) == 2
     params = args[0]
     body = args[1]
     return ["funktion", params, body]
 
 
-def do_aufrufen(envs,args):
+def do_funkaufrufen(envs, args):
     assert len(args) >= 1
     name = args[0]
     arguments = args[1:]
     # eager evaluation
-    values = [do(envs,arg) for arg in arguments]
+    values = [do(envs, arg) for arg in arguments]
 
-    func = envs_get(envs,name)
-    assert isinstance(func,list)
+    func = envs_get(envs, name)
+    assert isinstance(func, list)
     assert func[0] == "funktion"
     func_params = func[1]
     assert len(func_params) == len(values)
 
-    local_frame = dict(zip(func_params,values))
+    local_frame = dict(zip(func_params, values))
     envs.append(local_frame)
     body = func[2]
-    result = do(envs,body)
+    result = do(envs, body)
     envs.pop()
 
     return result
@@ -83,27 +83,67 @@ def do_abfolge(envs, args):
     return result
 
 
-OPS = {name.replace("do_", ""): func for (name, func) in globals().items() if name.startswith("do_")}
+###############################################
+# From here we add our calling infrastructure #
+###############################################
 
-def do(env, expr):
+
+OPS = {
+    name.replace("do_", ""): func
+    for (name, func) in globals().items()
+    if name.startswith("do_")
+}
+
+
+def do(envs, expr):
     # Lists trigger function calls.
     if isinstance(expr, list):
-      assert expr[0] in OPS, f"Unknown operation {expr[0]}"
-      func = OPS[expr[0]]
-      return func(env, expr[1:])
+        assert expr[0] in OPS, f"Unknown operation {expr[0]}"
+        func = OPS[expr[0]]
+        return func(envs, expr[1:])
     # Everything else returns itself
     else:
-      return expr
+        return expr
+
+
+# Environment
+def envs_get(envs, name):
+    assert isinstance(name, str)
+    for e in reversed(envs):
+        if name in e:
+            return e[name]
+    assert False, f"Unknown variable name {name}"
+
+
+def envs_set(envs, name, value):
+    assert isinstance(name, str)
+    envs[-1][name] = value
 
 
 def main():
-    assert len(sys.argv) == 2, "Usage: funcs-demo.py filename.gsc"
-    with open(sys.argv[1], "r") as source_file:
-        program = json.load(source_file)
-    assert isinstance(program, list)
-    envs = [{}]
-    result = do(envs, program)
-    print(f"=> {result}")
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        prog="lgl_interpreter.py",
+        description="Runs our little germal programming language.",
+        epilog="",
+    )
+    # nargs="+" means "at least one"
+    parser.add_argument("files", nargs="+", help="Specify lgl source files to run")
+    parser.add_argument(
+        "--trace", help="Log details of start and end times to FILENAME"
+    )
+    args = parser.parse_args()
+
+    # Run all files in order
+    for file in args.files:
+        file = os.path.join(abspath, file)
+        assert os.path.exists(file), f"File {file} does not exist"
+        with open(file, "r") as source_file:
+            program = json.load(source_file)
+        assert isinstance(program, list)
+        envs = [{}]
+        result = do(envs, program)
+        print(f"=> {result}")
 
 
 if __name__ == "__main__":
